1d1f65e7e1d90b3a25f2c65c5cd1d39bc6de80cc
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-04-01 11:01:27 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,22 @@
-# your code goes here
+require "pry"
+
+def begins_with_r(array)
+  
+  array.all? {|x| x.start_with?("r")}
+  
+end 
+
+
+def first_wa(array)
+  
+  new_array = []
+  array.each {|key, val| 
+    val.to_s
+    if x.start_with?("wa")
+      new_array.push(x)
+      binding.pry
+    end
+  }
+    new_array[0]
+  
+end
